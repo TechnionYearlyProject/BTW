ae62912a623237716dd853d20dbbf142d19abeb5
Guy-Rephaeli <guyre@campus.technion.ac.il>
2017-12-15 22:47:19 +0200

    Navigation Naive Implementation (untested)

--- BTW-Project/BTW-Navigation/src/main/java/il/ac/technion/cs/yp/btw/navigation/BTWNavigatorImp.java
+++ BTW-Project/BTW-Navigation/src/main/java/il/ac/technion/cs/yp/btw/navigation/BTWNavigatorImp.java
@@ -2,8 +2,15 @@ package il.ac.technion.cs.yp.btw.navigation;
 
 import com.google.common.graph.ImmutableValueGraph;
 import com.google.common.graph.MutableValueGraph;
+import com.google.common.graph.ValueGraph;
 import com.google.common.graph.ValueGraphBuilder;
 import il.ac.technion.cs.yp.btw.classes.Road;
+import org.jgrapht.alg.interfaces.AStarAdmissibleHeuristic;
+import org.jgrapht.alg.shortestpath.ALTAdmissibleHeuristic;
+import org.jgrapht.alg.shortestpath.AStarShortestPath;
+import org.jgrapht.graph.SimpleDirectedWeightedGraph;
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultWeightedEdge;
 
 import java.util.List;
 
@@ -13,7 +20,8 @@ import java.util.List;
 public class BTWNavigatorImp implements BTWNavigator {
 
     private final BTWDataBase Database;
-    private ImmutableValueGraph<Road, Long> graph;
+    private Graph<Road, DefaultWeightedEdge> heuristicGraph;
+    private AStarAdmissibleHeuristic<Road> heuristics;
 
     @Override
     public long calculateRouteTime(List<Road> route, double ratioSourceRoad, double ratioTargetRoad) {
@@ -22,26 +30,36 @@ public class BTWNavigatorImp implements BTWNavigator {
 
     public BTWNavigatorImp(BTWDataBase db) {
         this.Database = db;
-        initGraph();
+        this.heuristicGraph = initMinimumGraph();
+        this.heuristics = new ALTAdmissibleHeuristic<>(this.heuristicGraph, this.heuristicGraph.vertexSet());
     }
 
-    private void initGraph() {
-        MutableValueGraph<Road, Long> tmpGraph = ValueGraphBuilder.directed().build();
+    private Graph<Road, DefaultWeightedEdge> initMinimumGraph() {
+        SimpleDirectedWeightedGraph<Road, DefaultWeightedEdge> tmpGraph
+                = new SimpleDirectedWeightedGraph<>(DefaultWeightedEdge.class);
         Database.getAllTrafficLights()
-                .forEach(edge -> {
-                    Road src = edge.getSourceRoad();
-                    tmpGraph.putEdgeValue(src,
-                            edge.getDestinationRoad(),
-                            edge.getMinimumWeight().getWeightValue() + src.getRoadLength());
+                .forEach(trafficLight -> {
+                    Road src = trafficLight.getSourceRoad();
+                    Road dst = trafficLight.getDestinationRoad();
+                    if (! tmpGraph.containsVertex(src)) {
+                        tmpGraph.addVertex(src);
+                    }
+                    if (! tmpGraph.containsVertex(dst)) {
+                        tmpGraph.addVertex(dst);
+                    }
+                    DefaultWeightedEdge edge = tmpGraph.addEdge(src, dst);
+                    tmpGraph.setEdgeWeight(edge,
+                            trafficLight.getMinimumWeight().getWeightValue() + src.getMinimumWeight().getWeightValue());
                 });
-        graph = ImmutableValueGraph.copyOf(tmpGraph);
+        return tmpGraph;
     }
+
 
     @Override
-    public List<Road> navigate(Road source, Road target) {
+    public List<Road> navigate(Road src, Road dst) {
         //TODO - maybe update the graph in some way
-        return null;
+        // naive implementation
+        return new AStarShortestPath<>(this.heuristicGraph,
+                this.heuristics).getPath(src, dst).getVertexList();
     }
-
-
 }
