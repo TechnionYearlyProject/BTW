b0529c036f8a23e6a518bea9f952fc64e0e56730
Guy-Rephaeli <guyre@campus.technion.ac.il>
2017-12-21 16:45:26 +0200

    Added manifest for executable creation

---
+++ BTW-Project/BTW-Application/src/main/java/il/ac/technion/cs/yp/BTW.java
@@ -0,0 +1,11 @@
+package il.ac.technion.cs.yp;
+
+/**
+ * Hello world!
+ *
+ */
+public class BTW {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
