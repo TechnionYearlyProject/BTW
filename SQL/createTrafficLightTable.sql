f5f5ef4e019b06847eb6cad2fcf697b2b5425212
shaymichaeli <shaymichaeli@gmail.com>
2018-01-03 18:38:36 +0200

    just for creating tables in DB more easily

---
+++ SQL/createTrafficLightTable.sql
@@ -0,0 +1,7 @@
+﻿DROP TABLE IF EXISTS dbo.mapNameTrafficLight;
+CREATE TABLE mapNameTrafficLight(
+nameID varchar(50) NOT NULL,
+cordx smallint NOT NULL,
+cordy smallint NOT NULL,
+overload long,
+PRIMARY KEY(nameID));
