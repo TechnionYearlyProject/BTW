f67f450d7589b728b61aac73815d6e1f2231fbe4
shaymichaeli <shaymichaeli@gmail.com>
2018-01-08 13:51:19 +0200

    Create Heuristics table sql

---
+++ SQL/createHeuristicsTable.sql
@@ -0,0 +1,3 @@
+﻿CREATE TABLE testHeuristics (sourceID varchar(50) NOT NULL, targetID varchar(50) NOT NULL, overload bigint);
+INSERT INTO dbo.thirdHeuristics(sourceID,targetID) SELECT dbo.thirdRoad.nameID,a.nameID FROM dbo.testRoad CROSS JOIN dbo.testRoad AS a;
+SELECT * FROM dbo.testHeuristics;
