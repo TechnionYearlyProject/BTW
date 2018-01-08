3794e6760aaa6bf7c708114dc98f0bb68c6cc43b
Adam Elgressy <aelgressy3020@gmail.com>
2018-01-08 16:18:58 +0200

    Delete defunct classes

--- Stoplight.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by orelk_000 on 26/11/2017.
- */
-public interface Stoplight { //this is essentially an edge in the graph
-    Road getSourceRoad();
-    Road getTargetRoad();
-    Weight getStoplightWeightByHour(int hour, int minute);
-    Weight getCurrentStoplightWeight();
-}
--- Weight.java
+++
@@ -1,7 +0,0 @@
-/**
- * Created by orelk_000 on 26/11/2017.
- */
-public interface Weight {
-    //maybe add more to it
-    double getValue();
-}
