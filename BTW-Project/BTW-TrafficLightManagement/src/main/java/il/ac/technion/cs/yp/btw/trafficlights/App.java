60696c534df42dffa9133acc55be120d28cf9cea
Guy-Rephaeli <guyre@campus.technion.ac.il>
2018-01-12 14:01:38 +0200

    Added module for traffic lights management

---
+++ BTW-Project/BTW-TrafficLightManagement/src/main/java/il/ac/technion/cs/yp/btw/trafficlights/App.java
@@ -0,0 +1,13 @@
+package il.ac.technion.cs.yp.btw.trafficlights;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ BTW-Project/BTW-TrafficLightManagement/src/test/java/il/ac/technion/cs/yp/btw/trafficlights/AppTest.java
@@ -0,0 +1,14 @@
+package il.ac.technion.cs.yp.btw.trafficlights;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    @Test
+    public void testApp() {
+        Assert.assertTrue( true );
+    }
+}
